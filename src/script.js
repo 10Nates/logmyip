52a5aa2b2a7a69efea784c33182c475bbad03b3f
nathan <nathan@almostd.one>
2025-12-07 13:39:15 +0100

    Improvements to the look and feel of the website - dot opacity is now exponential decay, map zoom now zooms where you think it should, map zoom now works with keys

--- src/script.js
+++ src/script.js
@@ -35,16 +35,39 @@ silenceForm()
 // All of this is just for magnification
 var magnifier = 1
 const docEl = document.documentElement
+let docWidth = docEl.scrollWidth
+let docHeight = docEl.scrollHeight
+let scrollXP = .5
+let scrollYP = .5
+
+function setScrollPercent() {
+    // scroll percentage based on center of screen, i.e. 50% when fully zoomed out
+    scrollXP = (window.scrollX + (window.innerWidth / 2)) / docWidth;
+    scrollYP = (window.scrollY + (window.innerHeight / 2)) / docHeight;
+}
+
+function scrollViewport() {
+    let diffW = docEl.scrollWidth - docWidth
+    let diffH = docEl.scrollHeight - docHeight
+    docWidth = newWidth
+    docHeight = newHeight
+
+    window.scroll(window.scrollX + diffW * scrollXP, window.scrollY + diffH * scrollYP)
+}
 
 function decreaseMagnify() {
     magnifier *= 0.9
     if (magnifier < 1) magnifier = 1
+    setScrollPercent()
     docEl.style.setProperty("--magnify-scale", magnifier)
+    scrollViewport()
 }
 
 function increaseMagnify() {
     magnifier *= 1.1
+    setScrollPercent()
     docEl.style.setProperty("--magnify-scale", magnifier)
+    scrollViewport()
 }
 
 function openBigMap() {
@@ -63,3 +86,23 @@ document.addEventListener('DOMContentLoaded', () => {
     eID("magnify").onclick= openBigMap
     eID("mapbig").onclick = closeBigMap
 })
+
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case "+": // US/INT
+        case "=": // US/INT
+        case "*": // DIN
+        case "~": // DIN
+            increaseMagnify();
+            break;
+        case "-":
+        case "_":
+            if (e.shiftKey) magnifier = 1;
+            decreaseMagnify();
+            break;
+        case "Escape":
+            closeBigMap()
+            break;
+    }
+})
+
